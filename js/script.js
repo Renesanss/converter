591608cbf0ccef9672cc437f1c8aa0041c41759b
Renata Pientkowska <pientkowska.renata@gmail.com>
2022-11-03 22:26:38 +0100

    js - corrected

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-const calculateResult = () => {
+{
+    const calculateResult = () => {
     const amountElement = document.querySelector(".js-amount");
     const currencyToElement = document.querySelector(".js-currencyTo");
 
     const select = currencyToElement.value;
     const input = amountElement.value;
 
-    convert = input / select;
+    return input / select;
 };
 
 const init = () => {
+ 
     const formElement = document.querySelector(".js-form");
     formElement.addEventListener("submit", (event) => {
         event.preventDefault();
         calculateResult();
-const resultElement = document.querySelector(".js-result");
+        const result = calculateResult();
+        const resultElement = document.querySelector(".js-result");
 
-        resultElement.innerText = convert.toFixed(2);
+        resultElement.innerText = result.toFixed(2);
     });
-
 };
-
 init();
+}
 
 
 
